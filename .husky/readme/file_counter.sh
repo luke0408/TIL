a76ce5ce5d023d7ef6b9b610ad5c384a81b81fcf
zjvl324 <zjvl324@gmail.com>
2025-08-14 06:53:22 +0000

    feat: optimization directory counter

--- .husky/readme/file_counter.sh
+++ .husky/readme/file_counter.sh
@@ -6,9 +6,11 @@ export LANG="UTF-8"
 filepath=$(git rev-parse --show-toplevel)
 
 function generate_directory_file_counts() {
-  find . -type d ! -path "$filepath" | while read dir; do
-    dir_name=$(basename "$dir")
-    count=$(git ls-files "$dir" | grep '\.md$' | wc -l)
-    sed -i "s/__${dir_name}_DIRECTORY_COUNT__/${count}/g" "$filepath/README.md"
-  done
+  git ls-files '*.md' \
+    | grep -v '^README.md$' \
+    | cut -d/ -f1 \
+    | sort | uniq -c \
+    | while read count dir; do
+        sed -i "s/__${dir}_DIRECTORY_COUNT__/${count}/g" "$filepath/README.md"
+      done
 }
