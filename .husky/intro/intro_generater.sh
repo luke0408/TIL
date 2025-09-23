d35b996c0df9f4536f5c54b4853681fa9e471439
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-09-23 23:45:32 +0900

    fix: restore intro tree links for docusaurus

--- .husky/intro/intro_generater.sh
+++ .husky/intro/intro_generater.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANG="UTF-8"
+export LC_CTYPE=C.UTF-8
+export LC_ALL=C.UTF-8
+export LANG="C.UTF-8"
 filepath=$(git rev-parse --show-toplevel)
 intro="$filepath/docs/intro.md"
 
@@ -10,9 +10,9 @@ function generate_intro() {
   intro_template="$filepath/.husky/intro/intro_template.md"
   cp -f "$intro_template" "$intro"
 
-  generate_project_tree . |
-    proejct_tree_formatter |
-    LANG="UTF-8" perl -p0e 's/__PROJECT_TREE__/`cat`/se' -i "$intro"
+  generate_project_tree \
+    | proejct_tree_formatter \
+    | LANG="C.UTF-8" perl -p0e 's/__PROJECT_TREE__/`cat`/se' -i "$intro"
 
   generate_directory_file_counts
 }
