28e5e78a11378e1b4351370f475182e1e1017252
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-09-28 23:21:12 +0900

    Refactor homepage feature modules

--- src/theme/ColorModeToggle/index.tsx
+++ src/theme/ColorModeToggle/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import type {JSX} from 'react';
 import clsx from 'clsx';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import {translate} from '@docusaurus/Translate';
