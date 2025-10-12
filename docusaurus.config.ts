352400c2559f1d84d8c8187191674dc06022ff1c
zjvl324 <zjvl324@gmail.com>
2025-10-12 13:13:48 +0000

    stlye: logo update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Today I Learned',
   tagline: '매일은 아니더라도 꾸준하게',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/pets_dark.svg',
 
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
@@ -46,13 +46,14 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/pets_dark.svg',
     navbar: {
       hideOnScroll: true,
       title: 'luke0408/TIL',
       logo: {
         alt: 'luke0408',
-        src: 'img/logo.svg',
+        src: 'img/icon.png',
+        className: 'custom-navbar-icon'
       },
       items: [
         {
